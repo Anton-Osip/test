0bbeba089c0f746089501cd92d8191f6d23e6833
Anton-Osip <osipchika@gmail.com>
2024-07-16 20:51:48 +0300

    05 🥑 - Спринт 02, неделя 1

--- src/Button.tsx
+++
@@ -1,6 +0,0 @@
-import React, {ButtonHTMLAttributes} from "react";
-
-
-export const Button = ({title, onClick, className, disabled}: ButtonHTMLAttributes<HTMLButtonElement>) => {
-    return <button disabled = {disabled} className = {className} onClick = {onClick}>{title}</button>
-}
